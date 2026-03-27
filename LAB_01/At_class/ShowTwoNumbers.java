615c277826bf6a9be7e1c8adf08d24bc9b2f7abd
DangDinhCuong <dangcuongg25022004@gmail.com>
2026-03-27 16:57:10 +0700

    push EX 2.2.1 => 2.2.4

---
+++ LAB_01/At_class/ShowTwoNumbers.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JOptionPane;
+public class ShowTwoNumbers {
+public static void main(String[] args) {
+    // Khai báo kiểu String cho 2 biến số (dạng chuỗi)
+    String strNum1, strNum2; 
+    
+    // Khai báo kiểu String và khởi tạo giá trị cho thông báo
+    String strNotification = "You've just entered: "; 
+
+    // Bây giờ bạn có thể gán giá trị cho chúng mà không bị lỗi
+    strNum1 = JOptionPane.showInputDialog(null, 
+              "Please input the first number: ", "Input the first number",
+              JOptionPane.INFORMATION_MESSAGE);
+    strNotification += strNum1 + " and ";
+
+    strNum2 = JOptionPane.showInputDialog(null, 
+              "Please input the second number: ", "Input the second number",
+              JOptionPane.INFORMATION_MESSAGE);
+    strNotification += strNum2;
+
+    JOptionPane.showMessageDialog(null, strNotification,
+              "Show two numbers", JOptionPane.INFORMATION_MESSAGE);
+    
+    System.exit(0);
+}
+}
